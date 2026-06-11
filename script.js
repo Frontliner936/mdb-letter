ba4783e848ff8b8f51778a41727c25857aeb0cc2
Frontliner936 <frontlinertech@gmail.com>
2026-06-11 08:30:33 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+function generate() {
+
+const name =
+document.getElementById("name").value;
+
+const job =
+document.getElementById("job").value;
+
+document.getElementById("result").innerHTML =
+"Hello " + name +
+", your cover letter for " +
+job +
+" is ready.";
+
+}
